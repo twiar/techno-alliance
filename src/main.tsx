8e289db3e879cec763715727c8d2a0cbf8ef43fc
zhitenev.andr <zhitenev.andr@gmail.com>
2024-06-08 21:14:58 +0300

    changed to vite

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
