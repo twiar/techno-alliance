346e33d93a5d4680b482eab59f52aca19757f74d
zhitenev.andr <zhitenev.andr@gmail.com>
2024-07-28 15:32:16 +0300

    category section

---
+++ src/routes/category.tsx
@@ -0,0 +1,68 @@
+import { Outlet, Link } from "react-router-dom";
+import Header from "../components/Header/Header";
+import React from "react";
+import firstSectionImg from "../assets/1.webp";
+import Footer from "../components/Footer/Footer";
+
+export default function Linii() {
+  return (
+    <div className="h-dvh">
+      <Header isMainPage={false} />
+      <div className="w-full h-16 md:h-40"></div>
+
+      <section className="h-96 md:h-full-64 bg-primary flex justify-center items-end">
+        <div className="text-center mb-4 md:mb-16 relative z-20 text-white w-full md:w-1/2">
+          <h2 className="text-2xl md:text-5xl mb-4 md:mb-8 font-bold text-white">Технологические линии и оборудование для получения мясокостной муки</h2>
+
+          <div className="flex flex-col gap-1 md:gap-2">
+            <p className="text-base md:text-xl">Собственное производство</p>
+            <p className="text-base md:text-xl">Индивидуальный подход к проектированию и изготовлению</p>
+            <p className="text-base md:text-xl">Многолетний опыт</p>
+          </div>
+        </div>
+        <div className="absolute bg-cover bg-center bg-no-repeat w-full h-full bg-first-section-img transition duration-500 group-hover/item:scale-105 z-10 top-0 left-0 opacity-70 group-hover/item:opacity-50"></div>
+        <div className="absolute bg-cover bg-center bg-no-repeat w-full h-full bg-black z-0 top-0 left-0"></div>
+      </section>
+      <section className="bg-primary flex justify-center py-10">
+        <div className="flex justify-center flex-col">
+            <h2 className="text-black text-2xl md:text-5xl font-bold text-center mb-12">Продукция</h2>
+
+            <div className="flex flex-col gap-8 mb-10">
+              <div className="flex gap-8 flex-col md:flex-row">
+                <Link to="/product" className="block md:h-96 md:w-96 bg-white relative group/item overflow-hidden">
+                  <img alt="Здание решеток и КНС" src={firstSectionImg} className="object-cover w-full h-64 md:h-96 transition duration-500 group-hover/item:scale-105" />
+                  <p className="absolute w-full bottom-0 left-0 h-12 md:h-16 bg-white flex justify-center items-center">
+                    <span className="text-base md:text-lg font-medium text-black transition duration-500 group-hover/item:text-orange-500">Здание решеток и КНС</span>
+                  </p>
+                </Link>
+                <Link to="/product" className="block md:h-96 md:w-96 bg-white relative group/item overflow-hidden">
+                  <img alt="Здание решеток и КНС" src={firstSectionImg} className="object-cover w-full h-64 md:h-96 transition duration-500 group-hover/item:scale-105" />
+                  <p className="absolute w-full bottom-0 left-0 h-12 md:h-16 bg-white flex justify-center items-center">
+                    <span className="text-base md:text-lg font-medium text-black transition duration-500 group-hover/item:text-orange-500">Здание решеток и КНС</span>
+                  </p>
+                </Link>
+              </div>
+              <div className="flex gap-8 flex-col md:flex-row">
+                <Link to="/product" className="block md:h-96 md:w-96 bg-white relative group/item overflow-hidden">
+                  <img alt="Здание решеток и КНС" src={firstSectionImg} className="object-cover w-full h-64 md:h-96 transition duration-500 group-hover/item:scale-105" />
+                  <p className="absolute w-full bottom-0 left-0 h-12 md:h-16 bg-white flex justify-center items-center">
+                    <span className="text-base md:text-lg font-medium text-black transition duration-500 group-hover/item:text-orange-500">Здание решеток и КНС</span>
+                  </p>
+                </Link>
+                <Link to="/product" className="block md:h-96 md:w-96 bg-white relative group/item overflow-hidden">
+                  <img alt="Здание решеток и КНС" src={firstSectionImg} className="object-cover w-full h-64 md:h-96 transition duration-500 group-hover/item:scale-105" />
+                  <p className="absolute w-full bottom-0 left-0 h-12 md:h-16 bg-white flex justify-center items-center">
+                    <span className="text-base md:text-lg font-medium text-black transition duration-500 group-hover/item:text-orange-500">Здание решеток и КНС</span>
+                  </p>
+                </Link>
+              </div>
+            </div>
+        </div>
+      </section>
+      <Footer />
+      <div id="detail">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
